17e5d8feaa5a06f1446db4217cbf8d9fdceb506a
Andrey <andreytelushko@gmail.com>
2023-01-22 12:47:56 +0300

    Lesson 50

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -21,7 +21,7 @@ namespace HomeWork7
             for (int i = 0; i < array.GetLength(0); i++)
             {
                 for (int j = 0; j < array.GetLength(1); j++)
-                    array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+                    array[i, j] = Convert.ToInt32(new Random().Next(0, 100));
             }
 
             for (int i = 0; i < array.GetLength(0); i++)
