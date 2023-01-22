30ff9b805913669300226eb13af368009c8fc756
Andrey <andreytelushko@gmail.com>
2023-01-22 12:55:41 +0300

    Lesson 52

--- HomeWork7/Program.cs
+++ HomeWork7/Program.cs
@@ -11,35 +11,56 @@ namespace HomeWork7
     {
 
         static void Main()
-        {        
-                     
-            Console.WriteLine("Введите размеры массива");
-            int m = Convert.ToInt32(Console.ReadLine());
+        {
+
+            Console.WriteLine("введите количество строк");
             int n = Convert.ToInt32(Console.ReadLine());
-            int[,] array = new int[m, n];
+            Console.WriteLine("введите количество столбцов");
+            int m = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            int[,] numbers = new int[n, m];
+            FillArrayRandomNumbers(numbers);
+
+
+            for (int j = 0; j < numbers.GetLength(1); j++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                    array[i, j] = Convert.ToInt32(new Random().Next(0, 100));
+                double avarage = 0;
+                for (int i = 0; i < numbers.GetLength(0); i++)
+                {
+                    avarage = (avarage + numbers[i, j]);
+                }
+                avarage = avarage / n;
+                Console.Write(avarage + "; ");
+            }
+            Console.WriteLine();
+            PrintArray(numbers);
+
+
+
+            void FillArrayRandomNumbers(int[,] array)
+            {
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = new Random().Next(0, 10);
+                    }
+                }
             }
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            void PrintArray(int[,] array)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
-                    Console.Write(array[i, j] + "\t  ");
-                Console.WriteLine();
-            }
 
-            Console.WriteLine("Введите координаты");
-            int a = Convert.ToInt32(Console.ReadLine());
-            int b = Convert.ToInt32(Console.ReadLine());
-            if (a > m || b > n)
-                Console.WriteLine("такого числа нет");
-            else
-            {
-                object c = array.GetValue(a, b);
-                Console.WriteLine(c);
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    Console.Write("[ ");
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.Write("]");
+                    Console.WriteLine("");
+                }
             }
 
         }
